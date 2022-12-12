55dcad75a55c8b6038b7ad8f0ab3bc2124b5e67d
Gilang <gilangphb@gmail.com>
2022-12-12 17:48:05 +0700

    Menambahkan Credit.java

---
+++ Credit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author hp
+ */
+public class Credit extends Payment {
+    
+    private String number;
+    private String type;
+    private expDate;
+    
+    public void authorized(){
+        
+    }
+}
