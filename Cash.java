5e8131dffabe622720677ed82d0ea745162c0c60
Gilang <gilangphb@gmail.com>
2022-12-12 17:46:05 +0700

    Menambahkan Cash.java

---
+++ Cash.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author hp
+ */
+public class Cash extends Payment {
+    
+    private float cashTendered;
+    
+}
