0ffb5ee7ce0a1d9f2761e5ffadf6e9b3f79b1f1b
Gilang <gilangphb@gmail.com>
2022-12-12 17:46:43 +0700

    Menambahkan Check.java

---
+++ Check.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author hp
+ */
+public class Check extends Payment {
+    
+    private String name;
+    private String bankID;
+    
+    public void authorized(){
+        
+    }
+}
