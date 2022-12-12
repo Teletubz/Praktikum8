66ef73d08a1f5f431bd9a2a7ac8687a570dc9d80
Gilang <gilangphb@gmail.com>
2022-12-12 17:49:55 +0700

    Menambahkan Item.java

---
+++ Item.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author hp
+ */
+public class Item {
+    
+    private shippingWeight;
+    private String Description;
+    
+    public void getPriceForQuantity(){
+        
+    }
+    
+    public void getTax(){
+        
+    }
+    
+    public void inStock(){
+        
+    }
+}
